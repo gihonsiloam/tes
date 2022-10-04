33c00c230aba2356a0c1091506b4ae86afe149b4
gihonsiloam <51939999+gihonsiloam@users.noreply.github.com>
2022-10-04 15:43:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "aokwoakwokaowkaokwoaowk"
+    return "cek 123"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
