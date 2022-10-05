6d1e3ea0ab591c0525e1b1a3fdae90e96040aa7c
gihonsiloam <51939999+gihonsiloam@users.noreply.github.com>
2022-10-05 14:33:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "aaaaaaaaaaaaaa"
 
+@app.route('version')
+def get_version():
+    return "App version : 1.0"
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
